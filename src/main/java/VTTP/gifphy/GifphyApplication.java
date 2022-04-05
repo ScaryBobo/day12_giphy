48421536fb994ec0d2e29d4b104ccd3a9631033b
Leslie <leslielowgy@gmail.com>
2022-04-05 11:05:43 +0800

    first commit

---
+++ src/main/java/VTTP/gifphy/GifphyApplication.java
@@ -0,0 +1,13 @@
+package VTTP.gifphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifphyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifphyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/VTTP/gifphy/GifphyApplicationTests.java
@@ -0,0 +1,13 @@
+package VTTP.gifphy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifphyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
